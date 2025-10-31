b54ff883e7eaef47186633da779b8cd6f0aa9d14
ArjunNanavati <tootsie.nanavati@gmail.com>
2025-10-31 14:12:21 -0700

    apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -4,3 +4,17 @@
 #include "apex_code.h"
 
 using namespace std;
+
+bool isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i < number; ++i) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
